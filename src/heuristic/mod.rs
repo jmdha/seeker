8550f6d879db5f82ec3e5d4290b96fda816cbd53
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2024-05-24 16:04:18 +0200

    feat: measure heuristic time

--- src/heuristic/mod.rs
+++ src/heuristic/mod.rs
@@ -1,6 +1,8 @@
 pub mod constant;
 pub mod goal_count;
 
+use std::time::{Duration, Instant};
+
 use clap::ValueEnum;
 use pddllib::{state::State, task::Task};
 
@@ -14,6 +16,7 @@ pub struct Heuristic {
     kind: HeuristicKind,
     best_estimate: usize,
     estimates: usize,
+    time: Duration,
 }
 
 impl Heuristic {
@@ -22,14 +25,17 @@ impl Heuristic {
             kind,
             best_estimate: usize::MAX,
             estimates: 0,
+            time: Duration::default(),
         }
     }
 
     pub fn estimate(&mut self, task: &Task, state: &State) -> usize {
+        let t = Instant::now();
         let estimate = match self.kind {
             HeuristicKind::Constant => constant::estimate(task, state),
             HeuristicKind::GoalCount => goal_count::estimate(task, state),
         };
+        self.time += t.elapsed();
         if estimate < self.best_estimate {
             println!("New best heuristic estimate: {}", estimate);
             self.best_estimate = estimate;
@@ -41,6 +47,11 @@ impl Heuristic {
 
 impl Drop for Heuristic {
     fn drop(&mut self) {
-        println!("Heuristic estimates: {}", self.estimates);
+        println!(
+            "Heuristic estimates: {} ({:.2}s) ({:.2}/s)",
+            self.estimates,
+            self.time.as_secs_f64(),
+            self.estimates as f64 / self.time.as_secs_f64()
+        );
     }
 }
